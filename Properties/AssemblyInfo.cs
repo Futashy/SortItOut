cfbe373f2c43a40f9906c210ef870873f2b1f8d6
osmio <osmio@SHARKY-PC>
2023-03-14 15:15:39 +0300

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sort It Out")]
+[assembly: AssemblyDescription("Made for Darktide")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("None")]
+[assembly: AssemblyProduct("Sort It Out")]
+[assembly: AssemblyCopyright("Sharky")]
+[assembly: AssemblyTrademark("None")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("67830626-8231-4710-a87a-7b1b9078643a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ SortItOut.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace SortItOut
+{
+    class App
+    {
+        private static void Main()
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string file = (dir + @"\mod_load_order.txt");
+            DirectoryInfo di = new DirectoryInfo(dir);
+            DirectoryInfo[] dirs = di.GetDirectories();
+            string name = new DirectoryInfo(dir).Name;
+
+            int num = 1;
+
+            if (name != "mods")
+            {
+                Console.WriteLine("ERROR\nTHIS EXE MUST BE IN MODS FOLDER!!!");
+                Console.WriteLine("\nPress any key or close the window to exit.");
+                Console.ReadKey();
+            }
+            else
+            {
+                //Console.WriteLine($"Current directory is : {dir}\n");
+
+                using (StreamWriter ml = File.CreateText(file))
+                {
+                    foreach (var folder in dirs)
+                    {
+                        if (folder.Name != "base" && folder.Name != "dmf")
+                        {
+                            ml.WriteLine(folder);
+                            Console.WriteLine($"[{num++}] Added  \t - <{folder}>");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"[-] Skipped\t - <{folder}>");
+                        }
+                    }
+                }
+
+                Console.WriteLine("\nSorting completed!\n\nPress any key or close the window to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
