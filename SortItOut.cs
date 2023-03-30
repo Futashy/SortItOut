f2b4441d88f138a2f4845c868d45ca6fa8dff7d8
Sharky <osmiov.yura@gmail.com>
2023-03-30 15:24:43 +0300

    Hello World!

--- SortItOut.cs
+++ SortItOut.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace SortItOut
 {
@@ -14,40 +13,61 @@ namespace SortItOut
             DirectoryInfo di = new DirectoryInfo(dir);
             DirectoryInfo[] dirs = di.GetDirectories();
             string name = new DirectoryInfo(dir).Name;
-            List<string> list = new List<string>(); // just found out there's was a list, and it's better for this kinda tasks than array, hooray!
             int num = 1;
+
+            string[] topMod = { "LogMeIn" };
+            string[] endMod = { "animation_events", "ui_extension" };
+
+            List<string> topList = new List<string>();
+            List<string> list = new List<string>();
+            List<string> endList = new List<string>();
+
 
             //-----------------------------------------------------------------------------------------------------------------------------//
 
-            
-
             foreach (var folder in dirs)
             {
-                if (folder.Name != "base" & folder.Name != "dmf")
+                if (folder.Name == "base" || folder.Name == "dmf")
+                {
+                    //
+                }
+                else if (folder.Name.Contains(topMod[0]))
+                {
+                    topList.Add(folder.Name);
+                }
+                else if (folder.Name.Contains(endMod[0]) ^ folder.Name.Contains(endMod[1]))
+                { 
+                    endList.Add(folder.Name); 
+                } 
+                else 
                 {
                     list.Add(folder.Name);
                 }
             }
 
-            if (list.Contains("animation_events") && list.Contains("ui_extension"))
-            {
-                list.Remove("animation_events"); list.Add("animation_events"); // pushes it to the end of the list
-                list.Remove("ui_extension"); list.Add("ui_extension"); // same again
-            }
-
             if (name != "mods")
             {
                 Console.WriteLine("ERROR\nTHIS EXE MUST BE IN MODS FOLDER!!!\nPress any key or close the window to exit.");
-                Console.ReadKey();
+                    Console.ReadKey();
             }
             else
             {
                 using (StreamWriter ml = new StreamWriter(file))
                 {
-                    foreach (var item in list) // should be done in for loop, but who cares anyway?
+                    foreach (var item in topList)
                     {
                         ml.WriteLine(item);
-                        Console.WriteLine($"[{num++}] Added  \t\t\t - <{item}>");
+                        Console.WriteLine($"[{num++}] Added -\t(High Priority)\t - <{item}>");
+                    }
+                    foreach (var item in list)
+                    {
+                        ml.WriteLine(item);
+                        Console.WriteLine($"[{num++}] Added -\t\t\t - <{item}>");
+                    }
+                    foreach (var item in endList)
+                    {
+                        ml.WriteLine(item);
+                        Console.WriteLine($"[{num++}] Added -\t(Low Priority)\t - <{item}>");
                     }
                 }
             }
